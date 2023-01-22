041b0aa9ddc7cf4832b33b303d2d1d60f871db5d
Corentin Sallin <62545405+ThePotatoPvP@users.noreply.github.com>
2023-01-22 11:17:21 +0100

    Better slash help

--- src/Help.py
+++ src/Help.py
@@ -3,30 +3,29 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
+def _help_embed_maker(client: discord.Client, intput: str = 'base') -> discord.Embed:
+    if input == 'base':
+        embed = discord.Embed(color=0xf8e3e1)
+        for cog in client.cogs:
+            cmd_lst = ''
+            for command in client.get_cog(cog).get_commands():
+                if not command.hidden:
+                    cmd_lst += f'`{command.name}` | {command.brief}\n'
+            if not 'admin' in cog.lower() and not 'songd' in cog.lower():
+                embed.add_field(name=f"​", value=cmd_lst[:-1],inline=False)
+        embed.set_author(name=f"{client.user.display_name}'s help page",
+            icon_url=client.user.avatar_url)
+        return embed
+
 class Help(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.prefix = 'p!'
-
-
-    async def _help_embed_maker(self, input='base') -> discord.Embed:
-        if input == 'base':
-            embed = discord.Embed(color=0xf8e3e1)
-            for cog in self.client.cogs:
-                cmd_lst = ''
-                for command in self.client.get_cog(cog).get_commands():
-                    if not command.hidden:
-                        cmd_lst += f'`{command.name}` | {command.brief}\n'
-                if not 'admin' in cog.lower() and not 'songd' in cog.lower():
-                    embed.add_field(name=f"​", value=cmd_lst[:-1],inline=False)
-        embed.set_author(name=f"{self.client.user.display_name}'s help page",
-            icon_url=self.client.user.avatar_url)
 
     @commands.command(aliases=['h'], brief='Shows this page', display_name="help")
     async def help(self, ctx, *, input='base'):
-        await ctx.send(embed=self._help_embed_maker())
+        await ctx.send(embed=self._help_embed_maker(self.client))
 
     @app_commands.command(name='help', description='Shows all my commands')
     async def _help(self, interaction: discord.Interaction):
-        await interaction.response.send_message(embed=self._help_embed_maker)
+        await interaction.response.send_message(embed=_help_embed_maker(self.client))
 
