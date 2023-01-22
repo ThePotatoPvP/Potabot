ab74fb8e4f01a65cbffb703326ad4d4f73ffb929
Corentin Sallin <62545405+ThePotatoPvP@users.noreply.github.com>
2023-01-22 22:39:53 +0100

    test

--- src/Help.py
+++ src/Help.py
@@ -21,13 +21,13 @@ class Help(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(aliases=['h'], brief='Shows this page')
-    async def _help(self, ctx, *, entree='base'):
+    @commands.hybrid_command(aliases=['h'], brief='Shows this page')
+    async def help(self, ctx, *, entree='base'):
         await ctx.send(embed=_help_embed_maker(self.client))
 
-    @app_commands.command(name='help', description='Shows all my commands')
-    async def help(self, interaction: discord.Interaction):
-        await interaction.response.send_message(embed=_help_embed_maker(self.client))
+    #@app_commands.command(name='help', description='Shows all my commands')
+    #async def help(self, interaction: discord.Interaction):
+        #await interaction.response.send_message(embed=_help_embed_maker(self.client))
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Help(bot))
