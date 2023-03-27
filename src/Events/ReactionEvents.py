4bca89883b0e52303e46e114c12f66e694c21bae
ThePotatoPvP <corentin.sallin@gmail.com>
2023-03-27 12:40:17 +0200

    Adapted to new decorator options

--- src/Events/ReactionEvents.py
+++ src/Events/ReactionEvents.py
@@ -13,9 +13,13 @@ async def wednesday_event(client : discord.Client, message : discord.Message):
 
 @TriggeredEvent(keyword='Compile Error!',
                 user_id=510789298321096704,
-                guild_id=386474283804917760)
+                guild_id=386474283804917760,
+                case_sensitive=True,
+                chance=50,
+                channel_id=717005277706059783)
 async def latex_event(client : discord.Client, message : discord.Message):
     epic_latex = 'https://tenor.com/view/epic-latex-fail-latex-latex-fail-bad-latex-bad-tex-gif-26615851'
     shame = await message.channel.send(epic_latex)
     await asyncio.sleep(1200)
     await shame.delete()
+
