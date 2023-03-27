c7251bacdd9c022f547f9efcbd4f480cac596751
ThePotatoPvP <corentin.sallin@gmail.com>
2023-03-27 12:26:41 +0200

    Adapted for new decorator

--- src/Events/ReactionEvents.py
+++ src/Events/ReactionEvents.py
@@ -6,17 +6,16 @@ from discord.ext import commands
 import re, asyncio
 from src.Events.decorators import TriggeredEvent
 
+@TriggeredEvent(keyword='(mercredi|wednesday)', guild_id=386474283804917760)
 async def wednesday_event(client : discord.Client, message : discord.Message):
-    if message.author!=client.user and message.created_at.weekday() != 2 and re.match(r'.*(mercredi|wednesday).*', message.content.lower()):
+    if message.created_at.weekday() != 2:
         await message.channel.send('Fake news ! Nous ne sommes aucunement mercredi !')
 
+@TriggeredEvent(keyword='Compile Error!',
+                user_id=510789298321096704,
+                guild_id=386474283804917760)
 async def latex_event(client : discord.Client, message : discord.Message):
     epic_latex = 'https://tenor.com/view/epic-latex-fail-latex-latex-fail-bad-latex-bad-tex-gif-26615851'
-    if (message.channel.id == 717005277706059783) and (message.author.id == 510789298321096704) and ('Compile Error!' in message.content):
-        shame = await message.channel.send(epic_latex)
-        await asyncio.sleep(1200)
-        await shame.delete()
-
-@TriggeredEvent(keyword='foo')
-async def foo(client: discord.Client, message: discord.Message):
-    await message.channel.send('foo')
+    shame = await message.channel.send(epic_latex)
+    await asyncio.sleep(1200)
+    await shame.delete()
