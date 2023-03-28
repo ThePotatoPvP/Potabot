dba9de068ae68e808e8945ea2775155bdb8a6a30
Corentin Sallin <62545405+ThePotatoPvP@users.noreply.github.com>
2023-03-28 21:26:23 +0200

    Better structure to allow for multiple loops

--- main.py
+++ main.py
@@ -2,16 +2,7 @@
 import discord
 from discord.ext import commands
 import os
-import inspect
 import asyncio
-import importlib
-from concurrent.futures import ThreadPoolExecutor
-
-import src.Events.ReactionEvents
-import src.Events.ScheduledEvents
-
-from src.Music.MusicFunctions import MusicFunctions
-#from src.Music.Downloader import SongDownloader
 
 #Potato#8999
 #id = 694246129906483311
@@ -22,85 +13,25 @@ current_folder = os.path.dirname(os.path.abspath(__file__)) + "/"
 import pytz
 tz = pytz.timezone('Europe/Paris')
 
-################################
-# On met le prefixe de la joie #
-################################
 
-
-
-intents = discord.Intents().all()
-intents.members = True
-
-client  = commands.Bot(command_prefix = "p!", help_command=None, intents = intents)
+from src.Bots.Potabot import Potabot
+from src.Bots.EventBot import EventBot
 
 ###
-#   Events
+#   Booting bots
 ###
 
+import concurrent.futures
 
-
-@client.command()
-async def reload(ctx):
-    for cog in client.cogs():
-        client.remove_cog(cog)
-
-
-class Potabot(commands.Bot):
-    def __init__(self):
-        super().__init__(command_prefix="p!",help_command=None,intents=intents,)
-        self.cogs_to_quire = ["src.Help",
-                            "src.AdminCommands",
-                            "src.SFWInteractions",
-                            "src.Music.MusicFunctions"
-                            ]
-
-    async def on_ready(self):
-        await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name = "p!help"))
-        asyncio.create_task(scheduled_events_loop(client))
-        print("Potabot is online !")
-
-    async def on_message(self, message):
-        for name, func in inspect.getmembers(src.Events.ReactionEvents, inspect.iscoroutinefunction):
-            await func(client, message)
-        await client.process_commands(message)
-
-    async def setup_hook(self):
-        for cog in self.cogs_to_quire:
-            await self.load_extension(cog)
-        await client.tree.sync()
-
-    @client.event
-    async def on_member_join(member):
-        if "wati" in str(member.guild).lower():
-            role = discord.utils.get(member.guild.roles, id = 466166843132870667)
-            await member.add_roles(role)
-
-    @client.event
-    async def on_command_error(ctx, error):
-        cmd = ctx.command
-        if isinstance(error, (commands.MissingRequiredArgument, commands.BadArgument)):
-            example = f"{ctx.prefix}{cmd.name} {cmd.signature.replace('[bullshit]','')}"
-            await ctx.reply(f'Proper way to use {cmd.name} is {example}', delete_after=30)
-        elif isinstance(error, commands.CommandNotFound):
-            print(f'Failed on {ctx.message.content}')
-        else:
-            print(f'Error occured on {ctx.message.content} : \n {error}')
-
-async def scheduled_events_loop(client: discord.Client):
-    while True:
-        events = [func for name, func in inspect.getmembers(src.Events.ScheduledEvents, 
-                                                                predicate=lambda x: inspect.iscoroutinefunction(x))]
-        tasks = [event(client) for event in events]
-        print("Démarrage de la boucle pour les événements")
-        await asyncio.gather(*tasks)
-        await asyncio.sleep(1)
-
-
-
-
+def run_bot(token: str, bot: discord.Client):
+    bot.run(token)
 
 if __name__ == '__main__':
-    client = Potabot()
-    token = open('.token','r').read()
+    token = open('.token', 'r').read()
 
-    client.run(token)
+    ptb = Potabot()
+    evt = EventBot()
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.submit(run_bot, token, ptb)
+        executor.submit(run_bot, token, evt)
