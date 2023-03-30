6923acb2382cdaab8220667bc884c775f79cfc91
ThePotatoPvP <corentin.sallin@gmail.com>
2023-03-30 10:10:57 +0200

    Cleaner news channel and avoid repetitions

--- src/Events/ScheduledEvents.py
+++ src/Events/ScheduledEvents.py
@@ -30,13 +30,6 @@ async def wednesday(client: discord.Client):
     channel = client.get_channel(717298046144217099)
     await channel.send("https://i.kym-cdn.com/photos/images/newsfeed/001/091/264/665.jpg")
 
-@ScheduledEvent(hour=13, minute=00)
-async def watinewsmidi(client: discord.Client):
-    print('omjej c l event ')
-    imgz = src.Utils.reddit.grab_img('rienabranler')
-    channel = client.get_channel(734814108159049737)
-    await channel.send(f":rotating_light: Flash Info :rotating_light:\n Wati-Bonjour à tous, voici le titre de ce matin.")
-    await channel.send(imgz[0])
 
 @ScheduledEvent(hour=20, minute=00)
 async def watinewssoir(client: discord.Client):
