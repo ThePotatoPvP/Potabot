6d8a3dbdf98250483745ea4a8b458ac348bd921f
ThePotatoPvP <corentin.sallin@gmail.com>
2023-05-22 12:19:53 +0200

    Auto regex and more goofiness

--- src/Events/TriggeredEvents.py
+++ src/Events/TriggeredEvents.py
@@ -6,12 +6,21 @@ from discord.ext import commands
 import re, asyncio
 from src.Events.decorators import TriggeredEvent
 
-@TriggeredEvent(keyword='(mercredi|wednesday)', guild_id=386474283804917760)
+def mkrgx(keyword="", keywords=None, before:bool=1, after:bool=1):
+    if keywords is None: keywords = []
+    if keyword: keywords.append(keyword)
+    rgx = '('+ str.join('|', keywords)+')'
+    if before: rgx = '(|.*\s)'+ rgx
+    if after: rgx += '(\s.*|)'
+    return rgx + r'(\.|\?|\:|\;|\,|\*|\$|\!|\s)*($|\n)'
+
+
+@TriggeredEvent(rgx = mkrgx(keywords=['mercredi', 'wednesday']), guild_id=386474283804917760)
 async def wednesday_event(client : discord.Client, message : discord.Message):
     if message.created_at.weekday() != 2:
         await message.channel.send('Fake news ! Nous ne sommes aucunement mercredi !')
 
-@TriggeredEvent(keyword='Compile Error!',
+@TriggeredEvent(rgx=mkrgx(keyword='Compile Error!'),
                 user_id=510789298321096704,
                 guild_id=386474283804917760,
                 case_sensitive=True,
@@ -23,6 +32,18 @@ async def latex_event(client : discord.Client, message : discord.Message):
     await asyncio.sleep(1200)
     await shame.delete()
 
-@TriggeredEvent(keyword="quoi")
+@TriggeredEvent(rgx = mkrgx(keyword='quoi'))
 async def coiffeur(client : discord.Client, message : discord.Message):
     await message.reply(message.content.split('quoi')[0]+'feur')
+
+@TriggeredEvent(rgx = mkrgx(keywords=['oui', 'ui'], after=False))
+async def ouistiti(client: discord.Client, message : discord.Message):
+    await message.reply('stiti')
+
+@TriggeredEvent(rgx = mkrgx(keywords=['ouais', 'oe', 'we', 'ouai'], after=False))
+async def western(client: discord.Client, message : discord.Message):
+    await message.reply('stern')
+
+@TriggeredEvent(rgx = mkrgx(keywords=['non', 'nom'], after=False))
+async def nombril(client: discord.Client, message : discord.Message):
+    await message.reply('bril')
