f937c7066b57e7a941dac1d11185eeb4c57865de
Corentin Sallin <62545405+ThePotatoPvP@users.noreply.github.com>
2023-06-13 12:42:43 +0200

    backwards because whynot

--- src/Music/YTDLSource.py
+++ src/Music/YTDLSource.py
@@ -16,11 +16,7 @@ ytdl_format_options ={
 }
 
 ffmpeg_opts = {
-    'options': '-vn',
-    'reconnect': '1',
-    'reconnect_streamed' : '1',
-    'reconnect_delay_max': '5',
-    'http_persistent': '0'
+    'options': '-vn -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -http_persistent 0',
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
